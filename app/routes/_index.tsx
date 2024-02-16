dd0ae53f347769fbf2c67ff5cc296b44c6725696
Decyphr <blakeha94@gmail.com>
2024-02-16 16:58:06 -0700

    remove auth from homepage

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,42 +1,16 @@
-import {
-  json,
-  type LoaderFunctionArgs,
-  type MetaFunction,
-} from "@remix-run/node";
-import { Link, useLoaderData } from "@remix-run/react";
-import { Button } from "~/components/ui/button";
-
-import { requireUserId } from "~/lib/auth.server";
-import { generateRoute, routes } from "~/lib/routing";
+import { type MetaFunction } from "@remix-run/node";
 
 export const meta: MetaFunction = () => {
   return [
     { title: "Shadownet" },
-    { name: "description", content: "Welcome to the shadownet!" },
+    { name: "description", content: "Welcome to the shadownet." },
   ];
 };
 
-export async function loader({ request }: LoaderFunctionArgs) {
-  const userId = await requireUserId(request);
-
-  return json({ userId });
-}
-
 export default function Index() {
-  const { userId } = useLoaderData<typeof loader>();
-
   return (
     <main className="w-full h-screen flex flex-col gap-4 items-center justify-center">
       <h1>Welcome to Shadownet</h1>
-      <Button variant="outline" asChild>
-        <Link
-          to={generateRoute(routes.dashboard.user, {
-            "user-id": userId,
-          })}
-        >
-          Go to Dashboard
-        </Link>
-      </Button>
     </main>
   );
 }
